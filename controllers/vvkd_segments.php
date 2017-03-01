8682c0b241dafca8b828d55279b86ec973209562
RIAwolf <dunerunner>
2017-03-01 11:09:36 +0200

    added segments functionality

--- controllers/vvkd_segments.php
+++ controllers/vvkd_segments.php
@@ -1,11 +1,12 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 class JSON_API_vvkd_segments_Controller {
 
 	public function get_segments() {
 		global $wpdb;
 		$table_name = $wpdb->prefix . 'vvkd_optionssegments';
-		$results    = $wpdb->get_results( "SELECT * FROM $table_name;", OBJECT );
+		$results    = $wpdb->get_results( "SELECT * FROM $table_name ORDER BY `Number` ASC;", OBJECT );
 		$rows       = array();
 		foreach ( $results as $r ) {
 			$rows[] = new JSON_API_Options_Segments( $r );
@@ -21,7 +22,6 @@ class JSON_API_vvkd_segments_Controller {
 			return ( "ok" );
 		}
 		global $json_api;
-		$test = $json_api->query->get("Name");
 		$newSegment = new JSON_API_Options_Segments($json_api->query);
 		$newSegment->save();
 		return "done";
--- models/vvkd/options-segments.php
+++ models/vvkd/options-segments.php
@@ -1,32 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Martynas
- * Date: 2/26/2017
- * Time: 11:42
- */
 class JSON_API_Options_Segments {
     var $id;      // Integer
     var $Name;    // String
     var $Number;     // String
-    function JSON_API_Options_Segments ($wp_row) {
-        if ($wp_row) {
-            $this->import_wp_object($wp_row);
+    function JSON_API_Options_Segments ($row) {
+        if ($row) {
+            $this->import_wp_object($row);
         }
     }
 
-    function import_wp_object($wp_row) {
+    function import_wp_object($row) {
 
-        if(isset($wp_row->id)){
-	        $this->id = (int) $wp_row->id;
+        if(isset($row->id)){
+	        $this->id = (int) $row->id;
         }
 
-	    if(isset($wp_row->Name)) {
-		    $this->Name = $wp_row->Name;
+	    if(isset($row->Name)) {
+		    $this->Name = $row->Name ;
 	    }
 
-	    if(isset($wp_row->Number)) {
-		    $this->Number = $wp_row->Number;
+	    if(isset($row->Number)) {
+		    $this->Number = $row->Number;
 	    }
     }
 
